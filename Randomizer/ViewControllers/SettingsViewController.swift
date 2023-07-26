f3a37159c632e3512378f595b6c61a992f8074ca
Shalenikoff <KShalen@gmail.com>
2023-07-26 23:57:00 +0300

    Made code for using model in MainVC and SettingsVC

--- Randomizer/ViewControllers/SettingsViewController.swift
+++ Randomizer/ViewControllers/SettingsViewController.swift
@@ -9,20 +9,25 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    
     @IBOutlet var minValueTF: UITextField!
     @IBOutlet var maxValueTF: UITextField!
+    
+    var randomNumber: RandomNumber!
     
     var minimumValue: String!
     var maximumValue: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        minValueTF.text = minimumValue
-        maxValueTF.text = maximumValue
+        minValueTF.text = randomNumber.minimumValue.formatted()
+        maxValueTF.text = randomNumber.maximumValue.formatted()
     }
     
     @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func saveButtonPressed() {
         dismiss(animated: true)
     }
     
